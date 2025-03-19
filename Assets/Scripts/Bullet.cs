57b892e05d7d51261e1403f81f5b5f21c603de6d
nickdalhuisen <ndalhuisen@ziggo.nl>
2025-03-19 12:07:20 +0100

    bullet and begin of the enemy

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,45 @@ public class Bullet : MonoBehaviour
 {
     Rigidbody rb;
     [SerializeField] float bulletForce;
+
+    // Bullet delete timer
+    float bulletDeleteTimer;
+    bool bulletTimerStart;
     void Start()
     {
+        
         rb = GetComponent<Rigidbody>();
-        rb.linearVelocity = transform.forward * bulletForce;
+        // start Bullet delete timer
+       bulletTimerStart = true;
+
+       
+
     }
 
     
     void Update()
     {
+        // let bullet go forward
         rb.AddRelativeForce(new Vector3(bulletForce, 0, 0));
+
+        // Delete the bullet
+        if (bulletTimerStart)
+        {
+            bulletDeleteTimer += Time.deltaTime;
+        }
+        if(bulletDeleteTimer >= 3)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+      // check if enemy is hit and delete bullet
+      if(other.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            Debug.Log("Enemy hit");
+        }
     }
 }
