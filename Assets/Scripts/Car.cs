82da4e56e9bada53ac237acf8dca26e8a96f8f37
nickdalhuisen <ndalhuisen@ziggo.nl>
2025-03-20 09:43:44 +0100

    car missile

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
+using System;
+using System.ComponentModel.Design;
 
 public class Car : MonoBehaviour
 {
@@ -9,29 +10,59 @@ public class Car : MonoBehaviour
     [SerializeField] float Speed;
     [SerializeField] Vector3 prediction;
     [SerializeField] float RotateSpeed;
-    [SerializeField] 
+    CarTarget target;
+
+    float maxTargetDistance = 100;
+    float minTargetDistance = 5;
+
+    float deviationSpeed = 5;
+    float deviationAmount = 5;
+    private Vector3 standardPrediction, deviatedPrediction;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        target = FindFirstObjectByType<CarTarget>();
     }
 
   
-    void Update()
-    {
-        RotateCar();
-        var leadTimePercentage = Mathf.InverseLerp(Vector3.Distance(transform.position, target.transform.position));
-    }
+    
     private void FixedUpdate()
     {
         
         rb.AddRelativeForce(new Vector3(Speed, 0, 0));
 
+        var leadTimePercentage = Mathf.InverseLerp(minTargetDistance, maxTargetDistance, Vector3.Distance(transform.position, target.transform.position));
+
+        PredictPlayer(leadTimePercentage);
+
+        AddDeviation(leadTimePercentage);
+
+        RotateCar();
     }
     private void RotateCar()
     {
         var Location = prediction - transform.position;
 
         var rotation =Quaternion.LookRotation(Location);
-        rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, rotation, RotateSpeed * Time.deltaTime));
+       rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, rotation, RotateSpeed * Time.deltaTime));
+      
     }
+    private void PredictPlayer(float leadTimePercentage)
+    {
+        var predictionTime = Mathf.Lerp(0, minTargetDistance, leadTimePercentage);
+
+        standardPrediction = target.Rb.position + target.Rb.angularVelocity * predictionTime;
+
+
+    }
+
+    private void AddDeviation(float leadTimePercentage)
+    {
+        var deviation = new Vector3(Mathf.Cos(Time.time * deviationSpeed), 0, 0);
+
+        var predictionOffset = transform.TransformDirection(deviation) * deviationAmount * leadTimePercentage;
+
+        deviatedPrediction = standardPrediction + predictionOffset;
+    }
+   
 }
--- Assets/Scripts/CarTarget.cs
+++ Assets/Scripts/CarTarget.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class CarTarget : MonoBehaviour
 {
-    
+    Rigidbody rb;
+    float size = 10;
+    float speed = 10;
+    public Rigidbody Rb => rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
-
     
-    void Update()
+    public void Update()
     {
-        
+        var dir = new Vector3(Mathf.Cos(Time.time * speed) * size, Mathf.Sin(Time.time * speed) * size);
+
+        rb.angularVelocity = dir;
     }
 }
