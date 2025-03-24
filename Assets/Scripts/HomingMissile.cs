4dfba7663adcb12ef1f958c121d60c85ff5728bb
nickdalhuisen <ndalhuisen@ziggo.nl>
2025-03-24 09:20:41 +0100

    code changes

--- Assets/Scripts/HomingMissile.cs
+++ Assets/Scripts/HomingMissile.cs
@@ -17,15 +17,17 @@ public class HomingMissile : MonoBehaviour
     {
         Vector3 direction = target.position - rb.position;
         direction.Normalize();
-
+        // rotate rocket to player
         Vector3 amountToRotate = Vector3.Cross(direction, transform.forward) * Vector3.Angle(transform.forward, direction);
 
+        // let car fly forward
         rb.angularVelocity = -amountToRotate * rotateSpeed;
 
         rb.linearVelocity = transform.forward * speed;
     }
     private void OnTriggerEnter(Collider other)
     {
+        // check if rocket hit player
         if (other.CompareTag("Player"))
         {
             Destroy(gameObject);
