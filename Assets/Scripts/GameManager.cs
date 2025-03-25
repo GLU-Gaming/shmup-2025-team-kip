e58edb95d830002a8d47f5ff69af0c1e2af2d14d
JuliavdBerg <juliavandenberg11@gmail.com>
2025-03-25 11:18:50 +0100

    script changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,14 +5,43 @@ using UnityEngine.SceneManagement;
 using UnityEngine.VFX;
 public class GameManager : MonoBehaviour
 {
-    void Start()
+
+
+    [SerializeField] public int lives = 3;
+
+    [SerializeField] public GameObject life_1;
+    [SerializeField] public GameObject life_2;
+    [SerializeField] public GameObject life_3;
+
+    // all enemies
+    //[SerializeField] public GameObject carEnemy; //homing missile enemy
+    //[SerializeField] public GameObject toyboxEnemy; // shoots different items in a curve
+    //[SerializeField] public GameObject robotEnemy; // only shoots forward
+
+    [SerializeField] public int currentWave = 1;
+
+
+    [SerializeField] private List<GameObject> enemiesOnScreen = new();
+    private bool waveActive = true;
+
+
+    void Start ()
     {
-        
+
     }
 
-    void Update()
+    void Update ()
     {
-        
+        if ( currentWave == 1 && waveActive == true )
+        {
+            
+        }
+    }
+
+    void SpawnWave ( int difficulty )
+    {
+        waveActive = true;
+
     }
 
 }
