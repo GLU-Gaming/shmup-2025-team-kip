4589eb4761866fa677137b1d8d778e2b6da8ba11
nickdalhuisen <ndalhuisen@ziggo.nl>
2025-03-25 12:11:40 +0100

    speed a10

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
      Rigidbody  rb;
-    [SerializeField] private float verticalSpeed = 3;
-    [SerializeField] private float horizontalSpeed = 1;
+     private float verticalSpeed = 3;
+     private float horizontalSpeed = 1;
 
     [SerializeField] GameObject Bullet;
     [SerializeField] GameObject BulletSpawn;
-    
-  
+
+    [SerializeField] float speedamount;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -21,10 +21,10 @@ public class PlayerMovement : MonoBehaviour
     {
         // Player Movement
         verticalSpeed = Input.GetAxisRaw("Vertical");
-        rb.AddRelativeForce(new Vector3(0, verticalSpeed, 0 ));
+        rb.AddRelativeForce(new Vector3(0, verticalSpeed * speedamount, 0 ));
 
         horizontalSpeed = Input.GetAxisRaw("Horizontal");
-        rb.AddRelativeForce(new Vector3(horizontalSpeed, 0, 0));
+        rb.AddRelativeForce(new Vector3(horizontalSpeed * speedamount, 0, 0));
 
         // Spawn Bullet
         if (Input.GetKeyDown(KeyCode.Space))
