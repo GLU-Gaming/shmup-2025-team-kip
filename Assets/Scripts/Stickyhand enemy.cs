eed7572d4a6e7ac331a641db97dfab0b61702446
JuliavdBerg <juliavandenberg11@gmail.com>
2025-04-04 11:59:29 +0200

    start stickyhand

---
+++ Assets/Scripts/Stickyhand enemy.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Stickyhandenemy : MonoBehaviour
+{
+    void Start()
+    {
+        [SerializeField] float startTimer = 0;
+        [SerializeField] float speed;
+    }
+    void Update()
+    {
+        
+    }
+}
